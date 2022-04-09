92e46cf4e7f4b71ad1873879dcc792acb918ec3c
cllzkynk <cllzkynk@gmail.com>
2022-04-10 00:50:31 +0300

    10/04/2022

--- javascript/Practise/assets/js/53-error-handling.js
+++ javascript/Practise/assets/js/53-error-handling.js
@@ -1,9 +1,16 @@
-document.querySelector("#frmLogin").addEventListener("submit",(e)=>{
-e.preventDefault();
-
-    try{
- 
-    } catch(error){
- 
+document.querySelector("#frmLogin").addEventListener("submit", (e)=>{
+    e.preventDefault();
+    try {
+        let email = document.querySelector("#email").value;
+        let password = document.querySelector("#password").value;
+        if(!isEmail(email)) throw("Geçerli bir eposta giriniz"); 
+        if(!password) throw("Şifre boş geçilemez"); 
+        document.querySelector("#frmLogin").submit();
+    } catch (error) {
+       alert(error);
     }
 });
+function isEmail(email){
+    let reg = /\S+@\S+\.\S{2,5}$/;
+    return reg.test(email);
+}
